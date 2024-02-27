d400271ab9d77de4dca41c62f7ec67e499e19cd6
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-02-27 09:25:43 -0500

    dynamic volume slider

--- script.js
+++ script.js
@@ -47,13 +47,33 @@ function updateProgress() {
 
 // Click to seek within a video
 function setProgress(e) {
-  const newTime = e.offsetX / progressRange.offsetwidht;
-  progressRange.style.width = `${newTime * 100}%`;
-  video.currentTime = newTime * duration;
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
 }
 
 // Volume Controls --------------------------- //
 
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+  volumeIcon.className = "";
+  if (volume > 0.7) {
+    volumeIcon.classList.add("fas", "fa-volume-up");
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add("fas", "fa-volume-down");
+  } else if (volume === 0) {
+    volumeIcon.classList.add("fas", "fa-volume-off");
+  }
+}
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -64,3 +84,4 @@ video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
