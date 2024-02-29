b558ca8dc5242d4f7b8cf548657a5e9fbc4ebe3a
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-02-28 20:25:16 -0500

    volume mute/unmute feature

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
-
+let lastVolume = 1;
 // Volume Bar
 function changeVolume(e) {
   let volume = e.offsetX / volumeRange.offsetWidth;
@@ -73,7 +73,26 @@ function changeVolume(e) {
   } else if (volume === 0) {
     volumeIcon.classList.add("fas", "fa-volume-off");
   }
+  lastVolume = volume;
 }
+
+// Mute/Unmute
+function toggleMute() {
+  volumeIcon.classList = "";
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add("fas", "fa-volume-mute");
+    volumeIcon.setAttribute("title", "Unmute");
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.classList.add("fas", "fa-volume-up");
+    volumeIcon.setAttribute("title", "Mute");
+  }
+}
+
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -85,3 +104,4 @@ video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
