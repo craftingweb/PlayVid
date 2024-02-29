5ca839d489b540a6c511a34060a54df4a87a2bad
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-02-28 20:36:02 -0500

    playback speed

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector(".volume-range");
 const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
+const speed = document.querySelector(".player-speed");
 const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
@@ -94,7 +95,9 @@ function toggleMute() {
 }
 
 // Change Playback Speed -------------------- //
-
+function changeSpeed() {
+  video.playbackRate = speed.value;
+}
 // Fullscreen ------------------------------- //
 
 // Event Listeners
@@ -105,3 +108,4 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
